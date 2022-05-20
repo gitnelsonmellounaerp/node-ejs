f50f3738ee2022d2de111fe03cc41a5a94d24f8b
Nelson Mello <nelson.mello@sou.unaerp.edu.br>
2022-05-20 16:10:02 -0300

    first

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine", "ejs");
+
+//criando as paginas
+
+app.get("/", function(req, res){
+    //chmando conteudos de forma dinamica
+    const items = [
+        {
+            title: "D",
+            message: "esenvolver aplicações"
+        },
+        {
+            title: "E",
+            message: "JS usa javascript para renderizar html"
+        },
+        {
+            title: "F",
+            message: "oo"
+        }
+    ];
+
+    //outro exemplo de forma dinamica
+    const subtitle = "EJS é uma liguagem de criação de páginas HTML de forma dinamica."
+    res.render("pages/index", {
+        quality: items,
+        subtitle: subtitle, //olhar na index.ejs
+    });
+});
+
+app.get("/sobre", function(req, res){
+    res.render("pages/about");
+});
+
+app.listen(8080);
+console.log("Servidor funcionando...");
